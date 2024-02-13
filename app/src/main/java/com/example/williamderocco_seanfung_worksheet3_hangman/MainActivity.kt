f6172a8586d2b7b30ee88ed9d6d92118d78aea61
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-12 22:18:27 -0500

    code refactor -> moved keyboard init to helper function

--- app/src/main/java/com/example/williamderocco_seanfung_worksheet3_hangman/MainActivity.kt
+++ app/src/main/java/com/example/williamderocco_seanfung_worksheet3_hangman/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         val keyboardRow3: LinearLayout = findViewById(R.id.keyboardRow3)
         val keyboardRow4: LinearLayout = findViewById(R.id.keyboardRow4)
 
+        initializeKeyboardButtons(keyboardRow1, keyboardRow2, keyboardRow3, keyboardRow4)
+    }
+
+    private fun initializeKeyboardButtons(
+        keyboardRow1: LinearLayout,
+        keyboardRow2: LinearLayout,
+        keyboardRow3: LinearLayout,
+        keyboardRow4: LinearLayout
+    ) {
         val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
         // Loop to create buttons for each letter 'a' to 'z'
@@ -23,21 +33,18 @@ class MainActivity : AppCompatActivity() {
             button.text = char.toString()
             button.layoutParams = LinearLayout.LayoutParams(
                 100,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
             )
             button.setOnClickListener {
                 // Handle button click event
             }
 
             // Determine which row to add the button to
-            if (index < 8) {
-                keyboardRow1.addView(button)
-            } else if (index < 16) {
-                keyboardRow2.addView(button)
-            } else if (index < 24) {
-                keyboardRow3.addView(button)
-            } else {
-                keyboardRow4.addView(button)
+            when {
+                index < 8 -> keyboardRow1.addView(button)
+                index < 16 -> keyboardRow2.addView(button)
+                index < 24 -> keyboardRow3.addView(button)
+                else -> keyboardRow4.addView(button)
             }
         }
     }
