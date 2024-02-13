7c41f065ad804b38dceba9fd781ffd73570c662f
Fung-Sean <98567885+Fung-Sean@users.noreply.github.com>
2024-02-12 23:54:28 -0500

    Adding a view model to determine the state of the game. VM is still a work in progress

---
+++ app/src/main/java/com/example/williamderocco_seanfung_worksheet3_hangman/hangmanViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.williamderocco_seanfung_worksheet3_hangman
+
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "hangmanView"
+class hangmanViewModel : ViewModel() {
+    var answer = ""
+    var hint = ""
+    var usedLetters = mutableListOf<String>()
+    private var numHints = 0
+    private var maxTries = 6
+    var currentTries = 0
+    lateinit var underscoredLetters: String
+    var hangman: Int = R.drawable.state0
+    var playing = false
+    var win = false
+    var play = false
+    var firstHintShowed = false
+
+
+    private val wordDictionary: Map<String, List<String>> = mapOf(
+        "country" to listOf("america", "japan", "china", "mexico", "korea"),
+        "animal" to listOf("elephant", "zebra", "chicken", "anaconda", "hippo"),
+        "fruit" to listOf("apple", "grape", "kiwi", "pear", "orange"),
+        "sports" to listOf("basketball", "baseball", "soccer", "football", "hockey")
+    )
+
+    fun guess(letter: Char, hint: Boolean) {
+        usedLetters.add(letter.toString())
+        val indexLetter = mutableListOf<Int>()
+
+        answer .forEachIndexed { index, c ->  //c = char
+            if (c.equals(letter, ignoreCase = true)){
+                indexLetter.add(index)
+            }
+        }
+        var underscoredWord = underscoredLetters
+        indexLetter.forEach { index ->
+            val stringBuild = StringBuilder(underscoredWord).also { it.setCharAt(index, letter)}
+            underscoredWord = stringBuild.toString()
+        }
+
+        if (indexLetter.isEmpty()){
+            if(!hint)currentTries++
+            hangman = nextHangman()
+        }
+
+        if (currentTries == maxTries){
+            playing = false
+            win = false
+        }
+
+        underscoredLetters = underscoredWord
+        if(underscoredLetters.lowercase() == answer){
+            playing = false
+            win = true
+        }
+    }
+
+    fun nextHangman(): Int{
+        return when (currentTries) {
+            0 -> R.drawable.state0
+            1 -> R.drawable.state1
+            2 -> R.drawable.state2
+            3 -> R.drawable.state3
+            4 -> R.drawable.state4
+            5 -> R.drawable.state5
+            6 -> R.drawable.state6
+            else -> R.drawable.state6
+        }
+    }
+}
