727a922be1b24a7325d9b4edb54308a4492e221d
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-12 19:50:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/williamderocco_seanfung_worksheet3_hangman/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.williamderocco_seanfung_worksheet3_hangman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
